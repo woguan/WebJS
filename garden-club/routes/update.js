8271ef15fd87d22849a3e17929204012311fedb9
Wong Guan <woguan@users.noreply.github.com>
2017-02-05 01:50:18 -0800

    Update update.js

--- garden-club/routes/update.js
+++ garden-club/routes/update.js
@@ -2,6 +2,6 @@ var exec = require('child_process').exec;
 
 exports.view = function(req, res) {
 exec("cd ~/public/myweb && git pull origin master");
-console.log(req);
+console.log(res);
 res.send('Success');
 }
