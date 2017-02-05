823d1c6cad5925e326a6f784798115f4053a6897
Wong Guan <woguan@users.noreply.github.com>
2017-02-05 00:59:11 -0800

    Delete test.js

--- garden-club/public/test.js
+++
@@ -1,7 +0,0 @@
-var exec = require('child_process').exec;
-
-var child = exec('cat *.js | wc -l', function(error, stdout, stderr) {
-  if (error) console.log(error);
-  process.stdout.write(stdout);
-  process.stderr.write(stderr);
-});
